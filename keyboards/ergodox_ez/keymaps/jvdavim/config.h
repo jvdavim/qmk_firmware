420b3555b1dce169d349a676bf15f2aa41b6dddc
jvdavim <jvdavim@gmail.com>
2024-07-04 18:40:05 -0300

    update latest setup

--- keyboards/ergodox_ez/keymaps/jvdavim/config.h
+++ keyboards/ergodox_ez/keymaps/jvdavim/config.h
@@ -3,7 +3,6 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 #define ORYX_CONFIGURATOR
-#define IGNORE_MOD_TAP_INTERRUPT
 #define USB_SUSPEND_WAKEUP_DELAY 0
 #define CAPS_LOCK_STATUS
 #define LAYER_STATE_8BIT
--- keyboards/ergodox_ez/keymaps/jvdavim/keymap.c
+++ keyboards/ergodox_ez/keymaps/jvdavim/keymap.c
@@ -20,7 +20,7 @@
 #define SE_SECT_MAC ALGR(KC_6)
 
 enum custom_keycodes {
-  RGB_SLD = EZ_SAFE_RANGE,
+  RGB_SLD = SAFE_RANGE,
   ST_MACRO_0,
   ST_MACRO_1,
 };
@@ -31,24 +31,34 @@ enum {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ergodox_pretty(
-    KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_CAPSLOCK,                                    KC_PSCREEN,     KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,
-    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_LPRN,                                        KC_RPRN,        KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLASH,
-    KC_LSHIFT,      KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      TD(TD_ACCENT),
-    KC_LCTRL,       KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_LBRACKET,                                    KC_RBRACKET,    KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_EQUAL,
-    MO(1),          KC_LGUI,        KC_LALT,        KC_LEFT,        KC_RIGHT,                                                                                                       KC_UP,          KC_DOWN,        KC_RALT,        KC_RCTRL,       KC_RSHIFT,
+    KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_CAPS_LOCK,                                    KC_PSCR,     KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,
+    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_LPRN,                                        KC_RPRN,        KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,
+    KC_LSFT,      KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,      TD(TD_ACCENT),
+    KC_LCTL,       KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_LBRC,                                    KC_RBRC,    KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_EQUAL,
+    MO(1),          KC_LGUI,        KC_LALT,        KC_LEFT,        KC_RIGHT,                                                                                                       KC_UP,          KC_DOWN,        KC_RALT,        KC_RCTL,       KC_RSFT,
                                                                                                     KC_HOME,        KC_END,         KC_INSERT,      US_CCED,
-                                                                                                                    KC_PGUP,        KC_TRANSPARENT,
-                                                                                    KC_SPACE,       KC_BSPACE,      KC_PGDOWN,      KC_ESCAPE,      KC_DELETE,      KC_ENTER
+                                                                                                                    KC_PGUP,        TO(0),
+                                                                                    KC_SPACE,       KC_BSPC,      KC_PGDN,      TO(2),      KC_DELETE,      KC_ENTER
   ),
   [1] = LAYOUT_ergodox_pretty(
-    KC_ESCAPE,      KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_TRANSPARENT,                                 KC_PSCREEN,     KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
+    KC_ESCAPE,      KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_TRANSPARENT,                                 KC_PSCR,     KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
     ST_MACRO_0,     KC_EXLM,        KC_AT,          KC_LCBR,        KC_RCBR,        KC_PIPE,        KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_UP,          KC_7,           KC_8,           KC_9,           KC_ASTR,        KC_F12,
     ST_MACRO_1,     KC_HASH,        KC_DLR,         KC_LPRN,        KC_RPRN,        KC_GRAVE,                                                                       KC_DOWN,        KC_4,           KC_5,           KC_6,           KC_PLUS,        KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_PERC,        KC_CIRC,        KC_LBRACKET,    KC_RBRACKET,    KC_TILD,        KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_AMPR,        KC_1,           KC_2,           KC_3,           KC_BSLASH,      KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_PERC,        KC_CIRC,        KC_LBRC,    KC_RBRC,    KC_TILD,        KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_AMPR,        KC_1,           KC_2,           KC_3,           KC_BSLS,      KC_TRANSPARENT,
     KC_TRANSPARENT, KC_EQUAL,       KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_DOT,         KC_0,           KC_EQUAL,       KC_TRANSPARENT,
                                                                                                     TO(0),          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                                                     KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
+  ),
+ [2] = LAYOUT_ergodox_pretty(
+    KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_CAPS_LOCK,                                    KC_PSCR,     KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,
+    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_LPRN,                                        KC_RPRN,        KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_GRV,
+    KC_LSFT,      KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,      TD(TD_ACCENT),
+    KC_LGUI,       KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_LBRC,                                    KC_RBRC,    KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_EQUAL,
+    MO(1),          KC_LCTL,        KC_LALT,        KC_LEFT,        KC_RIGHT,                                                                                                       KC_UP,          KC_DOWN,        KC_RALT,        KC_RCTL,       KC_RSFT,
+                                                                                                    KC_HOME,        KC_END,         KC_INSERT,      US_CCED,
+                                                                                                                    KC_PGUP,        TO(0),
+                                                                                    KC_SPACE,       KC_BSPC,      KC_PGDN,      TO(2),      KC_DELETE,      KC_ENTER
   ),
 };
 
@@ -110,7 +120,7 @@ uint8_t layer_state_set_user(uint8_t state) {
   return state;
 };
 
-void accent(qk_tap_dance_state_t *state, void *user_data) {
+void accent(tap_dance_state_t *state, void *user_data) {
     if (state->count == 1) {
         register_code(KC_QUOTE);
         unregister_code(KC_QUOTE);
@@ -131,7 +141,7 @@ void accent(qk_tap_dance_state_t *state, void *user_data) {
     }
 }
 
-qk_tap_dance_action_t tap_dance_actions[] = {
+tap_dance_action_t tap_dance_actions[] = {
     [TD_ACCENT] = ACTION_TAP_DANCE_FN(accent),
 };
 

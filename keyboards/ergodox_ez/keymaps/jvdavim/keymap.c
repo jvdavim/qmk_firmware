2bf8ce338ef1da0cd77aa911bc9f6e65b2038451
jvdavim <jvdavim@gmail.com>
2024-10-23 09:14:54 -0300

    fix: missing backslash

--- keyboards/ergodox_ez/keymaps/jvdavim/keymap.c
+++ keyboards/ergodox_ez/keymaps/jvdavim/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESCAPE,      KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_NO,                                 KC_PSCR,     KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
     ST_MACRO_0,     KC_EXLM,        KC_AT,          KC_LCBR,        KC_RCBR,        KC_PIPE,        KC_NO,                                 KC_NO, KC_UP,          KC_NO,           KC_NO,           KC_NO,           KC_ASTR,        KC_F12,
     ST_MACRO_1,     KC_HASH,        KC_DLR,         KC_LPRN,        KC_RPRN,        KC_GRAVE,                                                                       KC_LEFT,        KC_DOWN,           KC_UP,           KC_RIGHT,           KC_PLUS,        KC_NO,
-    KC_NO, KC_PERC,        KC_CIRC,        KC_LBRC,    KC_RBRC,    KC_TILD,        KC_NO,                                 KC_NO, KC_AMPR,        KC_NO,           KC_NO,           KC_NO,           KC_NO,      KC_NO,
+    KC_NO, KC_PERC,        KC_CIRC,        KC_LBRC,    KC_RBRC,    KC_TILD,        KC_NO,                                 KC_NO, KC_AMPR,        KC_NO,           KC_NO,           KC_NO,           KC_BSLS,      KC_NO,
     KC_NO, KC_EQUAL,       KC_NO, KC_NO, KC_NO,                                                                                                 KC_NO, KC_DOT,         KC_NO,           KC_NO,       KC_NO,
                                                                                                     KC_NO,          KC_NO, KC_NO, KC_NO,
                                                                                                                     KC_NO, KC_NO,
